63db9782b69c85a51d5e1c3160170241f2fab3d1
Abdurrafey Khan <akhan@DE-intern-2472.local>
2018-11-01 15:01:38 -0500

    pseudocode

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Abdur Khan 
+Last updated 11/1/18
+This is the JavaScript document for the AIC New Tab Chrome Extension */
+
+/*
+Print salutation based on time (e.g. good morning if AM)
+Get local date and print in format [Day (name), Month day(date)] (e.g. Thursday, November 1st)
+Print local time
+*/
+
+/* Query AIC API for image
+Choose random image from database of images allowed based on CC0 Public Domain Designation tags
+Set up container for images (unsure if that's JS or other doc)
+Pull title and artist info for tombstone
+Print tombstone in bottom left region of page 
+*/
+
+/* Place download button near image
+When button is pressed, download image using user's browser
+*/
+
+
+/* function createParagraph() {
+  var para = document.createElement('p');
+  para.textContent = 'You clicked the button!';
+  document.body.appendChild(para);1
+}
+
+var buttons = document.querySelectorAll('button');
+
+for(var i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', createParagraph);
+}
+*/

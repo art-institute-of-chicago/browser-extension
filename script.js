64f8990dc8be4b2919071861b0ee0bff51bb63d0
Abdurrafey Khan <akhan@DE-intern-2472.local>
2018-11-15 14:57:21 -0600

    update source files

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ Print salutation based on time (e.g. good morning if AM, good afternoon if early
 Get local date and print in format [Day (name), Month day(date)] (e.g. Thursday, November 1st)
 Print local time
 */
-
+// Print greeting based on time
 function printDate() {
     var greeting;
     var time = new Date().getHours();
@@ -18,7 +18,62 @@ function printDate() {
     } else {
         greeting = "Good evening";
     }
+    //var printTime = new Date().get
     document.getElementById("demo").innerHTML = greeting;
+    //document.getElementByID("time").innerHTML = 
+}
+
+// Array to test random function
+var imgArray = ["bay_marseille.jpeg", 
+    "bedroom.png", 
+    "jatte.png", 
+    "lozenge.png", 
+    "shiva.png", 
+    "water-lilies.png", 
+    "wave.png"
+    ];
+
+// Array to test tombstone association function
+var titleArray = [
+    "The Bay of Marseille, Seen from L'Estaque, 1885",
+    "The Bedroom, 1889",
+    "A Sunday on La Grande Jatte -- 1884, 1884/86",
+    "Lozenge Composition with Yellow, Black, Blue, Red, and Gray, 1921",
+    "Shiva as Lord of the Dance (Nataraja), 10th/11th Century",
+    "Water Lilies, 1906",
+    'Under the Wave off Kanagawa (Kanagawa oki nami ura), also known as the Great Wave, from the series "Thirty-six Views of Mount Fuji (Fugaku sanjurokkei)"'
+    ]
+
+// Array to test artist association function
+var artistArray = [
+    "Paul Cézanne",
+    "Vincent van Gogh",
+    "Georges Seurat",
+    "Piet Mondrian",
+    "India, Tamil Nadu",
+    "Claude Monet",
+    "Katsushika Hokusai"
+]
+
+// Declare random number
+var imgIndex = Math.floor(Math.random() * imgArray.length);
+
+function randomImage(imgBank, path) {
+    path = path || "assets/";
+    var imgChoice = imgArray[imgIndex];
+    var imgPath = '<img src="' + path + imgChoice + '" alt - "">';
+    document.write(imgPath); document.close();
+}
+
+
+function tombstone() {
+    var tombstone = titleArray[imgIndex];
+    document.write(tombstone); document.close();
+}
+
+function artist() {
+  var artist = artistArray[imgIndex];
+  document.write(artist); document.close();
 }
 
 

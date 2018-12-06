1fc92e12567844c97915a5ac8502e6924affbda4
Abdurrafey Khan <akhan@DE-intern-2472.local>
2018-12-06 13:29:12 -0600

    update CSS

--- script.js
+++ script.js
@@ -2,67 +2,6 @@
 Last updated 12/6/18
 This is the JavaScript document for the AIC New Tab Chrome Extension */
 
-/*
-Print salutation based on time (e.g. good morning if AM, good afternoon if early PM, good evening if later, decide when to divide good evening/good night)
-Get local date and print in format [Day (name), Month day(date)] (e.g. Thursday, November 1st)
-Print local time
-*/
-// Print greeting based on time
-
-// Array to test random function
-var imgArray = ["bay_marseille.jpeg",
-    "bedroom.png",
-    "jatte.png",
-    "lozenge.png",
-    "shiva.png",
-    "water-lilies.png",
-    "wave.png"
-    ];
-
-// Array to test tombstone association function
-var titleArray = [
-    "The Bay of Marseille, Seen from L'Estaque, 1885",
-    "The Bedroom, 1889",
-    "A Sunday on La Grande Jatte -- 1884, 1884/86",
-    "Lozenge Composition with Yellow, Black, Blue, Red, and Gray, 1921",
-    "Shiva as Lord of the Dance (Nataraja), 10th/11th Century",
-    "Water Lilies, 1906",
-    'Under the Wave off Kanagawa (Kanagawa oki nami ura), also known as the Great Wave, from the series "Thirty-six Views of Mount Fuji (Fugaku sanjurokkei)"'
-    ]
-
-// Array to test artist association function
-var artistArray = [
-    "Paul Cézanne",
-    "Vincent van Gogh",
-    "Georges Seurat",
-    "Piet Mondrian",
-    "India, Tamil Nadu",
-    "Claude Monet",
-    "Katsushika Hokusai"
-]
-
-// Declare random number
-var imgIndex = Math.floor(Math.random() * imgArray.length);
-
-function randomImage(imgBank, path) {
-    path = path || "assets/";
-    var imgChoice = imgArray[imgIndex];
-    var imgPath = '<img src="' + path + imgChoice + '" alt - "">';
-    document.write(imgPath); document.close();
-}
-
-// Print title in tombstone
-function tombstone() {
-    var tombstone = titleArray[imgIndex];
-    document.write(tombstone); document.close();
-}
-
-// Print artist in tombstone
-function artist() {
-    getArtworkData();
-    var artist = artistPrint;
-    document.write(artist); document.close();
-}
 
 function getArtworkData() {
     var xmlhttp = new XMLHttpRequest();
@@ -80,8 +19,9 @@ function getArtworkData() {
         document.getElementById("tombstone").innerHTML = titlePrint;
         var imageID = myObj.data[0].image_id;
         var imageLink = '<img src = ' + '"https://www.artic.edu/iiif/2/' + imageID + '/full/600,/0/default.jpg">'
-        console.log(imageLink);
         document.getElementById("artwork").innerHTML = imageLink;
+        //var downloadLink = '<a href = ' + '"https://www.artic.edu/iiif/2/' + imageID + '/full/600,/0/default.jpg" download>'
+        //document.getElementById("download").innerHTML = downloadLink;
         }
     };
 

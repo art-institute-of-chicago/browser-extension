e32381b3cf2ca630e45115280234fbe2618f5621
nikhiltri <ntrivedi@artic.edu>
2023-06-28 13:29:12 -0500

    Use Measurement Protocol API to log Google Analytics [IP-190]

--- script.js
+++ script.js
@@ -304,16 +304,3 @@
             .replace(/-+$/, ''); // Trim - from end of text
     }
 })();
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-4351925-30']);
-_gaq.push(['_trackPageview']);
-
-(function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
